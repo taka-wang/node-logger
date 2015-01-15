4bce74f27c8961492640d68b8d4bbe28fae6b386
Taka Wang <wang.chia.ming@gmail.com>
2015-01-15 13:41:50 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var app = express();
+var mqtt = require('mqtt');
+var log = {
+    scale : "",
+    nearest: "",
+    qrcode: ""
+}
+
+app.get('/hello', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(log));
+  
+});
+
+
+var client  = mqtt.connect('mqtt://mqtt.cmwang.net');
+
+client.on("connect", function(){
+    client.subscribe("#", function(){
+        client.on("message", function(topic, payload, packet){
+            console.log("Received '" + payload + "' on '" + topic + "'");
+            switch (topic) {
+                case "/qr/":
+                    log["qrcode"] = payload.toString();
+                    break;
+                case "/ble/nearest/":
+                    log["nearest"] = payload.toString();
+                    break;
+                default:
+                    break;
+            }
+            client.publish("/log/", "hello world", function(){
+                console.log("message is published");
+            });
+        });
+    });
+
+});
+
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+})
