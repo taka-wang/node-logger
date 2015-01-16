fab16a80a654c4e04b21f6a01188c557220185c5
Taka Wang <wang.chia.ming@gmail.com>
2015-01-16 19:35:41 +0800

    add template page`

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+/**
+ * @Lab frontend
+ * @author Taka Wang
+ *
+*/
+var app = {
+    ctlMap : {
+        container: $("#container")
+    },
+    template : {
+        mytemplate: Handlebars.compile($("#mytemplate").html())
+    },
+    init: function() {
+        console.log("init");
+        app.bindEvent();
+        //render default
+        app.ctlMap.container.html(app.template.mytemplate({title: "Hello World"}));
+    },
+    bindEvent: function() {
+        console.log("bindEvent");
+    },
+    destroy: function() {
+        console.log("destroy");
+    }
+};
+
+
+$(function(){
+    app.init();
+});
---
+++ public/js/config.js
@@ -0,0 +1,8 @@
+host = '192.168.7.2';   // hostname or IP address
+port = 9001;
+topic = '#';        // topic to subscribe to
+useTLS = false;
+username = null;
+password = null;
+cleansession = true;
+reconnectTimeout  = 2000;
