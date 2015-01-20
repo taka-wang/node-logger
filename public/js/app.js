965d447e282a859e0031bb03c758f17fb32b5665
Taka Wang <wang.chia.ming@gmail.com>
2015-01-20 15:26:13 +0800

    code refactor

--- public/js/app.js
+++ public/js/app.js
@@ -24,50 +24,6 @@ var app = {
     }
 };
 
-var MQTT = {
-    client: null,
-    connect : function() {
-        
-        this.client = new Paho.MQTT.Client(
-                    window.location.hostname,
-                    config.port,
-                    "web_" + parseInt(Math.random() * 100,
-                    10));
-        this.client.onConnectionLost = MQTT.onConnectionLost;
-        this.client.onMessageArrived = MQTT.onMessageArrived;
-        if (config.username != null) {
-            this.options.userName = config.username;
-            this.options.password = config.password;
-        }
-        that = this;
-        this.client.connect({
-            timeout: config.timeout,
-            useSSL: config.useTLS,
-            cleanSession: config.cleansession,
-            onSuccess: function() {
-                that.client.subscribe(config.topic, {qos: 0});
-            },
-            onFailure: function (message) {
-                setTimeout(MQTT.connect, config.reconnectTimeout);
-            }
-        });
-    },
-    onConnect : function() {
-        console.log(this.client);
-        this.client.subscribe(config.topic, {qos: 0});
-    },
-    onConnectionLost: function(response) {
-        console.log(response);
-        setTimeout(MQTT.connect, config.reconnectTimeout);
-    },
-    onMessageArrived: function(message) {
-        var topic = message.destinationName;
-        var payload = message.payloadString;
-        console.log(topic + " : " + payload);
-    }
-};
-
-
 $(function(){
     app.init();
     MQTT.connect();
---
+++ public/js/mqtt.js
@@ -0,0 +1,37 @@
+var MQTT = {
+    client: null,
+    connect : function() {    
+        this.client = new Paho.MQTT.Client(
+                    window.location.hostname,
+                    config.port,
+                    "web_" + parseInt(Math.random() * 100,
+                    10));
+        this.client.onConnectionLost = MQTT.onConnectionLost;
+        this.client.onMessageArrived = MQTT.onMessageArrived;
+        that = this;
+        this.client.connect({
+            timeout: config.timeout,
+            useSSL: config.useTLS,
+            cleanSession: config.cleansession,
+            onSuccess: function() {
+                that.client.subscribe(config.topic, {qos: 0});
+            },
+            onFailure: function (message) {
+                setTimeout(MQTT.connect, config.reconnectTimeout);
+            }
+        });
+    },
+    onConnect : function() {
+        //console.log(this.client);
+        this.client.subscribe(config.topic, {qos: 0});
+    },
+    onConnectionLost: function(response) {
+        //console.log(response);
+        setTimeout(MQTT.connect, config.reconnectTimeout);
+    },
+    onMessageArrived: function(message) {
+        var topic = message.destinationName;
+        var payload = message.payloadString;
+        console.log(topic + " : " + payload);
+    }
+};
