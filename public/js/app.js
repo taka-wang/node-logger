43effe1876348f6ad152ba52c06e8ce81faf0699
Taka Wang <wang.chia.ming@gmail.com>
2015-01-20 14:44:41 +0800

    implement MQTT class

--- public/js/app.js
+++ public/js/app.js
@@ -24,7 +24,48 @@ var app = {
     }
 };
 
+var MQTT = {
+    client,
+    options : {
+        timeout: config.timeout,
+        useSSL: config.useTLS,
+        cleanSession: config.cleansession,
+        onSuccess: MQTT.onConnect,
+        onFailure: function (message) {
+            setTimeout(MQTT.connect, config.reconnectTimeout);
+        }
+    },
+    connect : function() {
+        MQTT.client = new Paho.MQTT.Client(
+                    window.location.hostname,
+                    config.port,
+                    "web_" + parseInt(Math.random() * 100,
+                    10));
+        MQTT.client.onConnectionLost = MQTT.onConnectionLost;
+        MQTT.client.onMessageArrived = MQTT.onMessageArrived;
+        if (config.username != null) {
+            MQTT.options.userName = config.username;
+            MQTT.options.password = config.password;
+        }
+        console.log("connecting");
+        MQTT.client.connect(MQTT.options);
+    },
+    onConnect : function() {
+        MQTT.client.subscribe(config.topic, {qos: 0});
+    },
+    onConnectionLost: function(response) {
+        console.log(response);
+        setTimeout(MQTT.connect, config.reconnectTimeout);
+    },
+    onMessageArrived: function(message) {
+        var topic = message.destinationName;
+        var payload = message.payloadString;
+        console.log(topic + " : " + payload);
+    }
+}
+
 
 $(function(){
     app.init();
+    MQTT.connect();
 });
--- public/js/config.js
+++ public/js/config.js
@@ -1,8 +1,12 @@
-host = '192.168.7.2';   // hostname or IP address
-port = 9001;
-topic = '#';        // topic to subscribe to
-useTLS = false;
-username = null;
-password = null;
-cleansession = true;
-reconnectTimeout  = 2000;
+var config = (function () { return {
+
+    port     : 9001,
+    topic    : "#",        // topic to subscribe to
+    useTLS   : false,
+    username : null,
+    password : null,
+    cleansession : true,
+    timeout  : 3,
+    reconnectTimeout : 2000
+
+};})();
