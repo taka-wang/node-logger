da7638318ff02a731d1c7869b06654ba30ac6f3f
Taka Wang <wang.chia.ming@gmail.com>
2015-01-22 17:31:06 +0800

    code refactor

--- public/js/app.js
+++ public/js/app.js
@@ -99,33 +99,23 @@ var app = {
         });
     },
     json2csv: function(JSONData, ReportTitle, ShowLabel) {
-        //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
         var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;      
         var CSV = '';    
-        //Set Report title in first row or line
-        //CSV += ReportTitle + '\r\n\n';
-        //This condition will generate the Label/Header
         if (ShowLabel) {
             var row = "";         
-            //This loop will extract the label from 1st index of on array
             for (var index in arrData[0]) {
-                //Now convert each value to string and comma-seprated
                 row += index + ',';
             }
             row = row.slice(0, -1);           
-            //append Label row with line break
             CSV += row + '\r\n';
         }
-        //1st loop is to extract each row
-        for (var i = 0; i < arrData.length; i++) {
+        for (var i = 0; i < arrData.length; i++) { //row
             var row = "";
-            //2nd loop will extract each column and convert it in string comma-seprated
-            for (var index in arrData[i]) {
+            for (var index in arrData[i]) { //col
                 row += '"' + arrData[i][index] + '",';
             }
             row.slice(0, row.length - 1);
-            //add a line break after each row
-            CSV += row + '\r\n';
+            CSV += row + '\r\n'; //add a line break after each row
         }
         if (CSV == '') {        
             alert("Invalid data");
@@ -134,15 +124,8 @@ var app = {
         
         //Generate a file name
         var fileName = "D2D_";
-        //this will remove the blank-spaces from the title and replace it with an underscore
         fileName += ReportTitle.replace(/ /g,"_");           
-        //Initialize file format you want csv or xls
-        var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
-        // Now the little tricky part.
-        // you can use either>> window.open(uri);
-        // but this will not work in some browsers
-        // or you will not get the correct file extension    
-        
+        var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);        
         //this trick will generate a temp <a /> tag
         var link = document.createElement("a");    
         link.href = uri;
