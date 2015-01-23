a325510c9ddc535797d33dd063b9ed18309d422e
Taka Wang <wang.chia.ming@gmail.com>
2015-01-23 10:30:42 +0800

    handle 404

--- index.js
+++ index.js
@@ -247,3 +247,21 @@ router.route("/reboot")
         });
     })
 
+app.use(function(req, res, next) {
+    res.status(404);
+    
+    // respond with html page
+    if (req.accepts('html')) {
+        res.render('404', { url: req.url });
+        return;
+    }
+
+    // respond with json
+    if (req.accepts('json')) {
+        res.send({ message: 'Not found' });
+        return;
+    }
+
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
+});
