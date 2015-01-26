b7f98389dc9a5f70e2b284d8191321eaf953b366
Taka Wang <wang.chia.ming@gmail.com>
2015-01-26 09:49:46 +0800

    fix selector

--- public/js/app.js
+++ public/js/app.js
@@ -325,7 +325,7 @@ var app = {
         });
 
         $("ul.sidebar-nav li").click(function() {
-            console.log($(this).attr("id"));
+            console.log($(this));
         });
 
         app.ctlMap.logger.click(function() {
