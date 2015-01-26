9f934f956394b478487cab8fb870ded55fdff5c7
Taka Wang <wang.chia.ming@gmail.com>
2015-01-26 14:52:24 +0800

    remove useless assets

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,6 @@
 */
 var app = {
     defaults: {
-        spinner: null,
         active:  "",
         beacons: {},
         items:   {},
@@ -263,39 +262,13 @@ var app = {
         link.click();
         document.body.removeChild(link);
     },
-    loadAnimation: function(div) {
-        var opts = {
-            lines: 13, // The number of lines to draw
-            length: 20, // The length of each line
-            width: 10, // The line thickness
-            radius: 30, // The radius of the inner circle
-            corners: 1, // Corner roundness (0..1)
-            rotate: 0, // The rotation offset
-            direction: 1, // 1: clockwise, -1: counterclockwise
-            color: '#FFF', // #rgb or #rrggbb or array of colors
-            speed: 1, // Rounds per second
-            trail: 60, // Afterglow percentage
-            shadow: false, // Whether to render a shadow
-            hwaccel: false, // Whether to use hardware acceleration
-            className: 'spinner', // The CSS class to assign to the spinner
-            zIndex: 2e9, // The z-index (defaults to 2000000000)
-            top: '50%', // Top position relative to parent in px
-            left: '50%' // Left position relative to parent in px
-        };
-        var target = document.getElementById(div);
-        var spinner = new Spinner(opts).spin(target);
-        return spinner;
-    },
     rende: function(type) {
         console.log("rende - " + type);
         var context = null;
         switch (type) {
             case "logger":
                 var logs = app.get_logs(function(context) {
-                    app.defaults.spinner = app.loadAnimation("Spin");
                     app.ctlMap.container.html(app.template.logger(context));
-                    app.defaults.spinner.stop();
-                    //spinner.stop();
                     /*
                     //bind datepicker
                     $("#datepicker").datepicker({
